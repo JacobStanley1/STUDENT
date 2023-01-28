524bdf9f3078fac53cd11fcbbb25784967899d9b
Jacob <122400947+JacobStanley1@users.noreply.github.com>
2023-01-27 20:52:07 -0500

    Update Card.java

--- src/exercise1/Card.java
+++ src/exercise1/Card.java
@@ -7,30 +7,31 @@ package exercise1;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author dancye
- * @author Paul Bonenfant May 2020
+ * @author Jacob Stanley
+ * @author Jacob Stanley Jan 27, 2023
  */
+
 public class Card {
 
    private String suit; //clubs, spades, diamonds, hearts
    private int value;//1-13
 
    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
-    /**
+    /
      * @return the suit
      */
     public String getSuit() {
         return suit;
     }
 
-    /**
+    /
      * @param suit the suit to set
      */
     public void setSuit(String suit) {
         this.suit = suit;
     }
 
-    /**
+    /
      * @return the value
      */
     public int getValue() {
@@ -42,5 +43,5 @@ public class Card {
      */
     public void setValue(int value) {
         this.value = value;
-    }  
+    }
 }
